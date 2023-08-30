dd859f6646500e94e91dbde6bb76dd251760a7d0
Janice Chen <janice.chen45@gmail.com>
2023-08-30 16:03:32 +0200

    [mod] by runTheMatrix.py

--- HLTrigger/Configuration/test/step2_RAW2DIGI_L1Reco_RECO.py
+++ HLTrigger/Configuration/test/step2_RAW2DIGI_L1Reco_RECO.py
@@ -4,10 +4,10 @@
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
 # with command line options: step2 --conditions 103X_dataRun2_Prompt_v2 -s RAW2DIGI,L1Reco,RECO --process reRECO -n 30 --data --era Run2_2018_pp_on_AA --eventcontent AOD --runUnscheduled --scenario pp --datatier AOD --repacked --filein file:out.root --fileout file:step2.root --no_exec
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+from Configuration.Eras.Era_Run2_2018_pp_on_AA_cff import Run2_2018_pp_on_AA
 
-from Configuration.StandardSequences.Eras import eras
-
-process = cms.Process('reRECO',eras.Run2_2018_pp_on_AA)
+process = cms.Process('reRECO',Run2_2018_pp_on_AA)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -15,7 +15,7 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.RawToDigi_DataMapper_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
@@ -23,17 +23,50 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = cms.untracked.int32(100000)
 )
 
+options = VarParsing.VarParsing ('analysis')
+options.parseArguments()
 # Input source
+filename = "outputDQM.root" if len(options.inputFiles)==0 else \
+           options.inputFiles[0]
+
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/hidata/HIRun2018/HITrackerVriginRaw/RAW/v1/000/325/813/00000/041AFBC1-FAF8-F240-BCEC-BEBE2FB67A32.root'),
+    fileNames = cms.untracked.vstring('file:'+filename),
     secondaryFileNames = cms.untracked.vstring()
 )
 
 process.options = cms.untracked.PSet(
-
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
 )
 
 # Production Info
@@ -49,10 +82,13 @@ process.outputClusters = cms.OutputModule("PoolOutputModule",
     compressionAlgorithm = cms.untracked.string('ZLIB'),
     compressionLevel = cms.untracked.int32(7),
     eventAutoFlushCompressedSize = cms.untracked.int32(31457280),
-    fileName = cms.untracked.string('file:step2clusters.root'),
+    fileName = cms.untracked.string('file:step2clusters'+filename.replace('outputDQM','')),
     outputCommands = cms.untracked.vstring(
-    'drop *',   
-	'keep *_*siStripClusters*_*_*'   
+    # 'keep *',   
+    'keep *_hltSiStripClusterizerForRawPrime_*_*',
+    'keep *_siStripZeroSuppression_*_*',
+    'keep *_siStripClusters_*_*',
+    'keep *_hltSiStripClusters2ApproxClusters_*_*'
     )
 )
 
@@ -61,6 +97,7 @@ process.outputClusters = cms.OutputModule("PoolOutputModule",
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '132X_dataRun3_HLT_v2', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_Prompt_v3', '')
 
 
 # Path and EndPath definitions
@@ -78,9 +115,15 @@ associatePatAlgosToolsTask(process)
 from Configuration.Applications.ConfigBuilder import MassReplaceInputTag
 # MassReplaceInputTag(process, new="rawDataMapperByLabel", old="rawDataCollector")
 
-#do not add changes to your config after this point (unless you know what you are doing)
-from FWCore.ParameterSet.Utilities import convertToUnscheduled
-process=convertToUnscheduled(process)
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.RecoTLR
+from Configuration.DataProcessing.RecoTLR import customisePostEra_Run2_2018_pp_on_AA 
+
+#call to customisation function customisePostEra_Run2_2018_pp_on_AA imported from Configuration.DataProcessing.RecoTLR
+process = customisePostEra_Run2_2018_pp_on_AA(process)
+
+# End of customisation functions
 
 
 # Customisation from command line
