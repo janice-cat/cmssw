2b5cea2ae83d1cf877834fa950e2464dfdb8d253
Janice Chen <janice.chen45@gmail.com>
2024-10-24 15:39:49 +0200

    [new] hybridZS hlt

---
+++ hybridZS/hltSubmitHITrackerNZS.py
@@ -0,0 +1,35 @@
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import getUsername
+JOBTAG = 'HITrackerNZS'
+USERNAME = getUsername()
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = JOBTAG
+config.General.workArea = 'WorkArea_HITrackerNZS'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.maxMemoryMB = 2500
+config.JobType.maxJobRuntimeMin = 250
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'hlt_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.numCores=4
+
+config.section_("Data")
+config.Data.inputDataset = '/HITrackerNZS/HIRun2023A-v1/RAW'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/'+USERNAME+'/HITrackerNZS_hybridZS/hlt/'
+config.Data.publication = False
+
+config.section_("Site")
+config.Site.whitelist = ['T2_US_Vanderbilt', 'T2_US_Nebraska', 'T2_CH_CERN']
+config.Site.storageSite = 'T2_CH_CERN'
+
+config.section_("Debug")
+config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=True']
