1534b05a7706d452e3e8e814a734cfc824bd2b12
Janice Chen <janice.chen45@gmail.com>
2024-10-27 11:51:40 +0100

    [mod] publish the datasets to tier2 server

--- hybridZS/hltSubmitHITrackerNZS.py
+++ hybridZS/hltSubmitHITrackerNZS.py
@@ -13,7 +13,7 @@ config.General.transferLogs = False
 
 config.section_("JobType")
 config.JobType.maxMemoryMB = 2500
-config.JobType.maxJobRuntimeMin = 250
+config.JobType.maxJobRuntimeMin = 330
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'hlt_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
@@ -24,12 +24,16 @@ config.Data.inputDataset = '/HITrackerNZS/HIRun2023A-v1/RAW'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 20
-config.Data.outLFNDirBase = '/store/group/phys_heavyions/'+USERNAME+'/HITrackerNZS_hybridZS/hlt/'
-config.Data.publication = False
+# config.Data.outLFNDirBase = '/store/group/phys_heavyions/'+USERNAME+'/HITrackerNZS_hybridZS/hlt/'
+config.Data.outLFNDirBase = '/store/user/'+USERNAME+'/HITrackerNZS_hybridZS/hlt/'
+config.Data.publication = True
+# config.Data.outputPrimaryDataset = 'HITrackerNZS_hybridZS_hlt'
+config.Data.outputDatasetTag = 'HITrackerNZS_hybridZS_hlt'
+# config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter'
 
 config.section_("Site")
-config.Site.whitelist = ['T2_US_Vanderbilt', 'T2_US_Nebraska', 'T2_CH_CERN']
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.whitelist = ['T2_US_Vanderbilt', 'T2_US_Nebraska', 'T2_CH_CERN', 'T2_US_MIT']
+config.Site.storageSite = 'T2_US_MIT'
 
 config.section_("Debug")
 config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=True']
